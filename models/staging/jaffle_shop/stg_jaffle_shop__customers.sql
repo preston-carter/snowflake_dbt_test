51cedbc635401bce9cd4f66dab92a49d636aea8c
Preston Carter <preston.alex.carter@gmail.com>
2025-01-30 22:17:22 +0000

    Updates to customer_orders and staging tables

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,13 @@
+with source as 
+(
+    select
+        *
+    from {{ source('jaffle_shop', 'customers') }}
+)
+
 select
     id as customer_id,
-    first_name,
-    last_name
-
-from from {{ source('jaffle_shop', 'customers') }}
+    last_name as surname,
+    first_name as givenname,
+    first_name || ' ' || last_name as full_name
+from customers
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,15 @@
+with source as 
+(
+    select
+        *
+    from {{ source('stripe', 'payment') }}
+)
+
 select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
-from {{ source('stripe', 'payment') }}
+    id as payment_id
+    , orderid as order_id
+    , paymentmethod as payment_method
+    , status as payment_status
+    , round(amount/100.0,2) as payment_amount
+    , created as created_at
+from source
